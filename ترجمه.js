998a734471f4a1e646c20db31cf308dcf1a68aca
logo805 <112271385+logo805@users.noreply.github.com>
2022-11-08 17:12:10 +0200

    Add files via upload
    xx

---
+++ ترجمه.js
@@ -0,0 +1,19 @@
+
+
+
+let arabic = document.getElementById("arabic");
+let english = document.getElementById("english");
+arabic.onclick = ()=>{
+    setlanugage("arabic");
+};
+english.onclick = ()=>{
+    setlanugage("english");
+};
+
+function setlanugage(getlanugage){
+if(getlanugage === "arabic"){
+
+}else if(getlanugage === "english"){
+    
+}  
+}
